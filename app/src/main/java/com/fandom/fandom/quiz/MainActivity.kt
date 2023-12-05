389985b7461d4e2d778b5c09e4012cdda9139cc5
Wojtek Maciejewski <wmaciejewski@fandom.com>
2023-12-05 12:36:56 +0100

    clean gradle

--- app/src/main/java/com/fandom/fandom/quiz/MainActivity.kt
+++ app/src/main/java/com/fandom/fandom/quiz/MainActivity.kt
@@ -3,7 +3,7 @@ package com.fandom.fandom.quiz
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.main_activity) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
