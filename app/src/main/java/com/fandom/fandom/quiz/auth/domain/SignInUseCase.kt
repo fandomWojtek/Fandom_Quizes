ca110773d881061791242fdd915e8c2834d31109
Wojtek Maciejewski <wmaciejewski@fandom.com>
2023-12-06 11:00:20 +0100

    fixes

--- app/src/main/java/com/fandom/fandom/quiz/auth/domain/SignInUseCase.kt
+++ app/src/main/java/com/fandom/fandom/quiz/auth/domain/SignInUseCase.kt
@@ -7,8 +7,8 @@ import com.onesignal.OneSignal
 class SignInUseCase(private val usersDb: UsersDb, private val userRepository: UserRepository, private val oneSignal: OneSignal) {
 
     suspend fun signIn(user: UserEntity) {
-        usersDb.addUser(user)
-        userRepository.saveUser(user)
+        val backendUser = usersDb.addUser(user)
+        userRepository.saveUser(backendUser)
         oneSignal.login(user.id)
     }
 }
--- app/src/main/java/com/fandom/fandom/quiz/remoteDb/UsersDb.kt
+++ app/src/main/java/com/fandom/fandom/quiz/remoteDb/UsersDb.kt
@@ -12,23 +12,19 @@ class UsersDb(private val firestore: FirebaseFirestore) {
         UserEntity(
             userName = it.getString(USERNAME) ?: "",
             userPhoto = it.getString(USER_PHOTO) ?: "",
-            lastActive = it.getLong(LAST_ACTIVE) ?: 0L,
+            lastActive = it.getLong(LAST_ACTIVE)?: 0L,
             id = it.id,
             points = it.getLong(POINTS)?.toInt() ?: 0
         )
     }
 
-    suspend fun addUser(user: UserEntity): UserEntity {
-        val document = firestore.collection(USERS_COLLECTION).add(user.mapOfElements()).await()
-        val user = user.copy(id = document.id)
-        return user
-    }
+    suspend fun addUser(user: UserEntity): UserEntity = user.copy(id = firestore.collection(USERS_COLLECTION).add(user.mapOfElements()).await().id)
 
     suspend fun getUserActiveInLastNMinutes(numberOfMinutes: Int = 5) = getAllUsers().filter {
-        it.isActive(numberOfMinutes)
+       it.isActive(numberOfMinutes)
     }
 
-    suspend fun updateCurrentTimeStampForUser(id: String) {
+    suspend fun updateCurrentTimeStampForUser(id:String) {
         firestore.collection(USERS_COLLECTION).document(id).update(LAST_ACTIVE, System.currentTimeMillis()).await()
     }
 }
