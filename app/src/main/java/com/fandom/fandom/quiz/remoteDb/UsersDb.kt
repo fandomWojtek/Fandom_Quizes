515760a61915b7b24ed1129fa945cf14b6ff5647
Wojtek Maciejewski <wmaciejewski@fandom.com>
2023-12-06 10:57:34 +0100

    fixes

--- app/src/main/java/com/fandom/fandom/quiz/remoteDb/UsersDb.kt
+++ app/src/main/java/com/fandom/fandom/quiz/remoteDb/UsersDb.kt
@@ -12,7 +12,7 @@ class UsersDb(private val firestore: FirebaseFirestore) {
         UserEntity(
             userName = it.getString(USERNAME) ?: "",
             userPhoto = it.getString(USER_PHOTO) ?: "",
-            lastActive = it.getLong(LAST_ACTIVE)?: 0L,
+            lastActive = it.getLong(LAST_ACTIVE) ?: 0L,
             id = it.id,
             points = it.getLong(POINTS)?.toInt() ?: 0
         )
@@ -21,14 +21,14 @@ class UsersDb(private val firestore: FirebaseFirestore) {
     suspend fun addUser(user: UserEntity): UserEntity {
         val document = firestore.collection(USERS_COLLECTION).add(user.mapOfElements()).await()
         val user = user.copy(id = document.id)
-      return user
+        return user
     }
 
     suspend fun getUserActiveInLastNMinutes(numberOfMinutes: Int = 5) = getAllUsers().filter {
-       it.isActive(numberOfMinutes)
+        it.isActive(numberOfMinutes)
     }
 
-    suspend fun updateCurrentTimeStampForUser(id:String) {
+    suspend fun updateCurrentTimeStampForUser(id: String) {
         firestore.collection(USERS_COLLECTION).document(id).update(LAST_ACTIVE, System.currentTimeMillis()).await()
     }
 }
